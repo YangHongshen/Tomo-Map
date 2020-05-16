44f25eb7e05d1788fdc552ab6814927adc86bc60
yanghongshen <yang2819281@gmail.com>
2020-05-16 22:39:38 +1200

    Login/Register Page warning messages

--- mapApp/views.py
+++ mapApp/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import login, authenticate
 from mapApp import models, forms
-import json
+
+
+def first_value(dictionary):
+    first_key_value = next(iter(dictionary.values()))[0]
+    return first_key_value
 
 
 # Create your views here.
@@ -13,6 +17,7 @@ def index_page(request):
 
 
 def login_page(request):
+    messages = []
     if request.method == 'POST':
         form = forms.LoginForm(request.POST)
         if form.is_valid():
@@ -20,50 +25,43 @@ def login_page(request):
             password = form.cleaned_data["password"]
             try:
                 user = models.UserModel.objects.get(email=email)
+                if user.password == password:
+                    messages.append("Congrats, you have logged in successfully")
+                    # TODO: auth system
+                else:
+                    messages.append("Sorry, passsword is invalid")
             except ObjectDoesNotExist:
-                return render(request, "mapApp/login.html", {'email_messages': ["User not found"], 'form': form})
-            if user.password == password:
-                return HttpResponse("Congrats, you have logged in successfully")
-            else:
-                return render(request, "mapApp/login.html",
-                              {'password_messages': ["Sorry, passsword is invalid"], 'form': form})
+                messages.append("User not found")
         else:
-            messages = form.errors
-            try:
-                email_messages = messages['email']
-            except KeyError:
-                email_messages = ""
-            try:
-                password_messages = messages['password']
-            except KeyError:
-                password_messages = ""
-            return render(request, "mapApp/login.html",
-                          {'email_messages': email_messages, 'password_messages': password_messages, 'form': form})
+            messages.append(first_value(form.errors.values()))
     else:
         form = forms.LoginForm()
-        return render(request, "mapApp/login.html", {'form': form})
+    return render(request, "mapApp/login.html", {'messages': messages, 'form': form})
 
 
 def register_page(request):
+    messages = []
     if request.method == 'POST':
         form = forms.RegisterForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data["email"]
+            # TODO: Add complexity check
             password1 = form.cleaned_data["password1"]
             password2 = form.cleaned_data["password2"]
             if password1 != password2:
-                return HttpResponse("Sorry, two password does not match")
+                messages.append("Sorry, two password does not match")
             else:
                 models.UserModel.objects.create(
                     email=email,
                     password=password1
                 )
-                return HttpResponse("Congrats, your have registered successfully")
+                messages.append("Congrats, your have registered successfully")
+                # TODO: Hop to Login page with auth
         else:
-            return HttpResponse("Sorry, form is invalid")
+            messages.append(first_value(form.errors))
     else:
         form = forms.RegisterForm()
-        return render(request, "mapApp/register.html", {'form': form})
+    return render(request, "mapApp/register.html", {'form': form, 'messages': messages})
 
 
 def reset_page(request):
