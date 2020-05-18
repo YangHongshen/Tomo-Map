33f6842cd13a436888dfcadba74a45abb5d16fe5
yanghongshen <yang2819281@gmail.com>
2020-05-18 20:13:24 +1200

    switch to click event instead of submit

--- mapApp/static/script/master.js
+++ mapApp/static/script/master.js
@@ -1,15 +1,11 @@
-function confirmPassword(form_id) {
+function confirmPassword(button_id) {
     var p1 = document.getElementsByName("password1")[0].value;
     var p2 = document.getElementsByName("password2")[0].value;
-    if (true) {
-    // TODO: change form empty fields alert
-    pass
-    }
-    else if (p1 != p2) {
+    if (p1 != p2) {
         var warn = document.getElementsByClassName('form__warning')[0];
         warn.classList.remove('d-none');
     }
     else {
-        document.getElementById(form_id).submit();
+        document.getElementById(button_id).click();
     }
 }
