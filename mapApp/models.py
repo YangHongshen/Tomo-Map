ae087c60779cdb91e147abd08ffc6521e07d5317
yanghongshen <yang2819281@gmail.com>
2020-06-17 22:17:58 +1200

    Implement ForeignKey Restraint

--- mapApp/models.py
+++ mapApp/models.py
@@ -6,7 +6,14 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 class FriendList(models.Model):
-    username = models.CharField(max_length=150)
-    friend = models.CharField(blank=True, default="", max_length=150)
-    friend_request_received = models.CharField(blank=True, default="", max_length=150)
-    friend_request_sent = models.CharField(blank=True, default="", max_length=150)
+    username = models.ForeignKey(User, related_name="+", on_delete=models.CASCADE)
+    friend = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    friend_request_received = models.ForeignKey(User, blank=True, related_name="+", default="",
+                                                on_delete=models.CASCADE)
+    friend_request_sent = models.ForeignKey(User, blank=True, related_name="+", default="", on_delete=models.CASCADE)
+
+
+class NotificationList(models.Model):
+    notification = models.TextField(max_length=150)
+    notification_sent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    notification_received = models.ForeignKey(User, on_delete=models.CASCADE)
